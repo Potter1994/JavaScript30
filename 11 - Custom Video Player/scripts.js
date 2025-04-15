533df67441759e05d8722ce0226c2d5671bbbb29
Potter <sky753159852@gmail.com>
2025-04-15 14:58:38 +0800

    重寫一次

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,79 +1,44 @@
 const video = document.querySelector(".player__video");
-const [playButton, rewinderButton, forwardButton] =
+const [playButton, ...skipButtons] =
   document.querySelectorAll(".player__button");
-const [volumeInput, speedRateInput] =
-  document.querySelectorAll(".player__slider");
-console.dir(video);
-const progressBar = document.querySelector(".progress__filled");
+const rangeInputs = document.querySelectorAll('input[type="range"]');
+const progressFilled = document.querySelector(".progress__filled");
 const progress = document.querySelector(".progress");
-const progressWidth = progress.clientWidth;
+
 let isMouseDown = false;
 
-video.volume = volumeInput.value;
-const { duration } = video;
-
-// function handleVideoPlay() {
-//   if (video.paused) {
-//     video.play();
-//   } else {
-//     video.pause();
-//   }
-// }
-
-// 1. adjust namespace & use string to method call
 function togglePlay() {
   const method = video.paused ? "play" : "pause";
+  playButton.textContent = video.paused ? "||" : "►";
   video[method]();
 }
 
-// 2. lose pause icon
-function updateIcon() {
-  const currentIcon = video.paused ? "►" : "||";
-  playButton.textContent = currentIcon;
+function changeProgressFilledWidth(e) {
+  const progressPercent = `${(video.currentTime / video.duration) * 100}%`;
+  progressFilled.style.setProperty("flex-basis", progressPercent);
 }
 
-// function handleVolume() {
-//   video.volume = this.value;
-// }
-
-// function handleSpeedRate() {
-//   video.playbackRate = this.value;
-// }
-
-// 3. use string to method call
-function handleRangeUpdate() {
+function changeRangeProperty() {
   video[this.name] = this.value;
 }
 
-function handleDragProgress(e) {
+function handleSkip() {
+  video.currentTime += parseInt(this.dataset.skip);
+  console.log(this);
+}
+
+function slideProgress(e) {
   if (!isMouseDown) return;
-  const percent = e.offsetX / progressWidth;
-  const dragTime = duration * percent;
-  video.currentTime = dragTime;
+  const totalWidth = progress.offsetWidth;
+  const progressPercent = e.offsetX / totalWidth;
+  video.currentTime = video.duration * progressPercent;
 }
 
-function handleClickRewindOrForward() {
-  const skip = this.dataset.skip;
-  if (skip) {
-    video.currentTime += parseInt(skip);
-  }
-}
-
-function calculateProgress() {
-  const { currentTime } = video;
-  const progressPercent = (currentTime / duration) * 100;
-  progressBar.style.flexBasis = `${progressPercent}%`;
-}
-
-playButton.addEventListener("click", togglePlay);
-
-video.addEventListener("pause", updateIcon);
-video.addEventListener("play", updateIcon);
 video.addEventListener("click", togglePlay);
-video.addEventListener("timeupdate", calculateProgress);
-
-volumeInput.addEventListener("input", handleRangeUpdate);
-speedRateInput.addEventListener("input", handleRangeUpdate);
+playButton.addEventListener("click", togglePlay);
+video.addEventListener("timeupdate", changeProgressFilledWidth);
+rangeInputs.forEach((i) => i.addEventListener("input", changeRangeProperty));
+skipButtons.forEach((i) => i.addEventListener("click", handleSkip));
 
 progress.addEventListener("mousedown", () => {
   isMouseDown = true;
@@ -81,7 +46,4 @@ progress.addEventListener("mousedown", () => {
 progress.addEventListener("mouseup", () => {
   isMouseDown = false;
 });
-progress.addEventListener("mousemove", handleDragProgress);
-
-rewinderButton.addEventListener("click", handleClickRewindOrForward);
-forwardButton.addEventListener("click", handleClickRewindOrForward);
+progress.addEventListener("mousemove", slideProgress);
